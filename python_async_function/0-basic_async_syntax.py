e45c05df6e914698dffd3d96449c1461436e42b3
Alexandre Guerin <113639808+AlexandreGRN@users.noreply.github.com>
2024-05-03 10:10:08 +0000

    Fix: new style

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -5,8 +5,8 @@ asynchronous coroutine
 import asyncio
 import random
 
-async def wait_random(max_delay=10):
-    """ asynchronous coroutine """
-    seconds = random.uniform(0, max_delay)
-    await asyncio.sleep(seconds)
-    return seconds
+async def wait_random(max_delay: int = 10) -> float:
+    """ asynchronous coroutine  """
+    ran = random.uniform(0, float(max_delay))
+    await asyncio.sleep(ran)
+    return float(ran)
--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -6,7 +6,7 @@ import asyncio
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-async def wait_n(n, max_delay):
+async def wait_n(n: int, max_delay: int) -> typing.List[float]:
     """ concurrent coroutine """
     result = [wait_random(max_delay) for i in range(n)]
     return sorted(await asyncio.gather(*result))
