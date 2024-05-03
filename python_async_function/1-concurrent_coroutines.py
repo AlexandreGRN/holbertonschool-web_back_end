7dff9f69cdb5a012bbba723879ccad03d58892ac
Alexandre Guerin <113639808+AlexandreGRN@users.noreply.github.com>
2024-05-03 10:11:42 +0000

    Fix: fixed return value of task 1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -7,6 +7,11 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> typing.List[float]:
-    """ concurrent coroutine """
-    result = [wait_random(max_delay) for i in range(n)]
-    return sorted(await asyncio.gather(*result))
+    """ takes 2 int, n, max_delay and output a list of waited time """
+    file = __import__('0-basic_async_syntax')
+    arr: typing.List[float] = []
+    i: int = 0
+    while (i < n):
+        arr.append(await file.wait_random(max_delay))
+        i += 1
+    return sorted(arr)
