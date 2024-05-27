e6745862ce2bba64ecd5cf5ba98e9be1ac229df9
AlexandreGRN <tulkiidra@gmail.com>
2024-05-27 10:03:15 +0100

    Feat: Added a fifo cache system

---
+++ caching/1-fifo_cache.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" implementation of a FIFO caching system """
+BasicCache = __import__('0-basic_cache').BasicCache
+
+
+class FIFOCache(BasicCache):
+    """ FIFO cache class """
+    def __init__(self):
+        """ constructor """
+        super().__init__()
+
+    def put(self, key, item):
+        """ add an item to the cache """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            if len(self.cache_data) > self.MAX_ITEMS:
+                keys = list(self.cache_data.keys())
+                first = keys[0]
+                print("DISCARD: {}".format(first))
+                del self.cache_data[first]
+
+    def get(self, key):
+        """ get an item by key """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
