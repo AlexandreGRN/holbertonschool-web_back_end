9f4ed165cf1638bfadaf675f813b07909a64cd1c
Alexandre Guerin <113639808+AlexandreGRN@users.noreply.github.com>
2024-06-23 12:56:58 +0000

    Feat: add filter logger using re.sub function

---
+++ personal_data/README.md
@@ -0,0 +1,1 @@
+Personal Datas
---
+++ personal_data/filtered_logger.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""" filtered logger """
+
+from typing import List, Tuple
+import re
+
+
+def filter_datum(fields: List[str],
+                 redaction: str, message: str, separator: str) -> str:
+    """ this function hide the data from the main """
+    patern = "|".join(fields)
+    return re.sub(f'({patern})=.*?{separator}', r'\g<1>' + "="
+                  + redaction + separator, message)
