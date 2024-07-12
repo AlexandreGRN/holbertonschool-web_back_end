b0cbc2567f37035e7f7f1f7099f62e62b57f104e
Alexandre Guerin <113639808+AlexandreGRN@users.noreply.github.com>
2024-07-12 09:29:11 +0000

    Fix: changed a bit for pycodestyle

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -2,8 +2,6 @@
 """
 Route module for the API
 """
-
-# Assign the result of auth.current_user(request) to request.current_user
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
@@ -14,30 +12,38 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-auth = getenv("AUTH_TYPE")
-if auth == "auth":
-    from api.v1.auth.auth import Auth
-    auth = Auth()
-if auth == "basic_auth":
-    from api.v1.auth.basic_auth import BasicAuth
-    auth = BasicAuth()
+auth = None
+auth_type = os.environ.get("AUTH_TYPE")
+if auth_type:
+    if auth_type == "basic_auth":
+        from api.v1.auth.basic_auth import BasicAuth
+        auth = BasicAuth()
+    elif auth_type == "session_auth":
+        from api.v1.auth.session_auth import SessionAuth
+        auth = SessionAuth()
+    else:
+        from api.v1.auth.auth import Auth
+        auth = Auth()
 
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler """
+    """ Not found handler
+    """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
-def not_authorized(error) -> str:
-    """ unauthorized handler """
+def unauthorized(error) -> str:
+    """ Not found handler
+    """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """ forbidden handler """
+    """ Not found handler
+    """
     return jsonify({"error": "Forbidden"}), 403
 
 
@@ -69,4 +75,4 @@ def before_request():
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+    app.run(host=host, port=port, debug=True)
