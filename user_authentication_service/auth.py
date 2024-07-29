6919ba33d415b534b786fb4245a5034b4b7b3e62
Alexandre Guerin <113639808+AlexandreGRN@users.noreply.github.com>
2024-07-29 11:41:03 +0000

    Feat: added register_user function

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -3,8 +3,27 @@
 AUTH file
 """
 import bcrypt
-
+from db import DB
+from sqlalchemy.orm.exc import NoResultFound
 
 def _hash_password(password: str) -> bytes:
     """ hash password for security """
     return bcrypt.hashpw(password.encode("UTF-8"), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ register user """
+        try:
+            self._db.find_user_by(email=email)
+
+            raise ValueError(f'User {email} already exists')
+        except NoResultFound:
+            hash_pwd = _hash_password(password)
+            return self._db.add_user(email=email, hashed_password=hash_pwd)
