fca160b779f035ccd22ea6abaeb14f9a869bde4e
Alexandre Guerin <113639808+AlexandreGRN@users.noreply.github.com>
2024-07-29 11:59:03 +0000

    Fix: fixed import error

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -7,6 +7,8 @@ from db import DB
 from user import User
 import sqlalchemy
 from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+import uuid
 
 
 def _hash_password(password: str) -> bytes:
