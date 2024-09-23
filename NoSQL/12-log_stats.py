bed98057be5a8568f8be76d8e423761f99623926
Alexandre Guerin <113639808+AlexandreGRN@users.noreply.github.com>
2024-09-23 15:08:52 +0000

    Refacto: changed main function

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,31 +1,15 @@
 #!/usr/bin/env python3
-"""log stats from collection
+"""
+a script that provides some stats about a nginx dump logs stored in mongo xxx
 """
 from pymongo import MongoClient
 
 
-METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
-
-def log_stats(mongo_collection, option=None):
-    """ script that provides some stats about Nginx logs stored in MongoDB
-    """
-    items = {}
-    if option:
-        value = mongo_collection.count_documents(
-            {"method": {"$regex": option}})
-        print(f"\tmethod {option}: {value}")
-        return
-
-    result = mongo_collection.count_documents(items)
-    print(f"{result} logs")
-    print("Methods:")
-    for method in METHODS:
-        log_stats(nginx_collection, method)
-    status_check = mongo_collection.count_documents({"path": "/status"})
-    print(f"{status_check} status check")
-
-
 if __name__ == "__main__":
-    nginx_collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
-    log_stats(nginx_collection)
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    c = client.logs.nginx
+    method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("{} logs\nMethods:".format(c.count_documents({})))
+    for m in method:
+        print("\tmethod {}: {}".format(m, c.count_documents({"method": m})))
+    print("{} status check".format(c.count_documents({"method": "GET", "path": "/status"})))
