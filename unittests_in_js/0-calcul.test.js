71994a4e878967aca9d660e5b979e501b72fe180
Alexandre Guerin <113639808+AlexandreGRN@users.noreply.github.com>
2024-12-03 09:10:07 +0000

    Fix: fixed typo in test

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -4,7 +4,7 @@ const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
     it('should return sum of integers', () => {
-        assert.equal(calculateNumber(1, 3), 8);
+        assert.equal(calculateNumber(1, 3), 4);
         assert.equal(calculateNumber(1, 3.7), 5);
         assert.equal(calculateNumber(1.2, 3.7), 5);
         assert.equal(calculateNumber(1.5, 3.7), 6);
