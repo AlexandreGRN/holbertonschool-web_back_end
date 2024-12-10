22acc80de971095a5e9c779832c7b5aaf59af9ec
Alexandre Guerin <113639808+AlexandreGRN@users.noreply.github.com>
2024-12-10 09:16:02 +0000

    Fix: changed describe and it naming

--- unittests_in_js/8-api/api.test.js
+++ unittests_in_js/8-api/api.test.js
@@ -1,14 +1,18 @@
 const request = require('request');
-const chai = require('chai')
-const { expect } = require('chai');
+const chai = require('chai');
 
-describe('API test', () => {
-      it('respond with 200 code and body', (done) => {
-        request('http://localhost:7865', 'GET', (err, res, body) => {
-          if (err) throw err;
-          expect(res.statusCode).to.equal(200);
-          expect(body).to.equal('Welcome to the payment system');
-        });
+describe('basic integration testing', () => {
+  describe('GET /', () => {
+    it('endpoint GET /', (done) => {
+      request('http://localhost:7865', (error, response, body) => {
+        if (error) {
+          done(error);
+          return;
+        }
+        chai.expect(response.statusCode).to.equal(200);
+        chai.expect(body).to.equal('Welcome to the payment system');
         done();
+      });
     });
+  });
 });
